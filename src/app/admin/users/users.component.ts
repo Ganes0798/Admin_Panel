1c1bf8568f2374e62c06f6058d634459ac233760
Ganes0798 <mganesh120798@gmail.com>
2024-04-19 19:14:38 +0530

    worked the login api and dashboard changes

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {} from '@angular/material/'
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
