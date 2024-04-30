12e2a048be8f689216a4b8cab2f585a5f832e814
Ganes0798 <mganesh120798@gmail.com>
2024-04-30 12:07:23 +0530

    User Add and Get Api Finished

--- src/app/admin/users/user-input/user-input.component.ts
+++ src/app/admin/users/user-input/user-input.component.ts
@@ -1,25 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { ReactiveFormsModule, UntypedFormBuilder, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatOptionModule } from '@angular/material/core';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog'
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [MatDialogModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatTableModule, MatPaginatorModule],
+  imports: [CommonModule,MatDialogModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatTableModule, MatPaginatorModule, ReactiveFormsModule, MatSelectModule, MatOptionModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.scss'
 })
 export class UserInputComponent implements OnInit, AfterViewInit{
+  userForm: any;
 
   
 
-  constructor(private dialog: MatDialog){}
+  constructor(private dialog: MatDialog, ){}
 
   ngOnInit(): void{
+
+   
 
   }
 
