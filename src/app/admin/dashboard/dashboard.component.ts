0342034df8ef72da8da1fe8b66b2bc15703b7df1
Ganes0798 <mganesh120798@gmail.com>
2024-10-01 14:52:40 +0530

    Dashboard and Product page Ui Design

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { Chart, registerables } from 'chart.js';
-import {MatTableModule} from '@angular/material/table'
+import {MatTableDataSource, MatTableModule} from '@angular/material/table'
 import { CommonModule } from '@angular/common';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatDrawer } from '@angular/material/sidenav';
+import { CommonServiceService } from '../../common-service/common-service.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 Chart.register(...registerables);
 
@@ -13,7 +17,7 @@ Chart.register(...registerables);
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule,MatCardModule, MatIconModule, MatListModule, MatTableModule],
+  imports: [CommonModule,MatCardModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
@@ -21,6 +25,8 @@ Chart.register(...registerables);
 
 export class DashboardComponent implements OnInit {
   listData: any;
+
+  constructor(private _commonService: CommonServiceService, private _snackBar: MatSnackBar){}
 
   ngOnInit(): void {
      this.listData = [
